25e02b182328e8bba7aa94cea3c75a5c24869237
Manicka <mapichov@microsoft.com>
2019-09-18 13:34:20 +0200

    issue_32611

---
+++ issue_32611/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Net;
+using System.IO;
+
+namespace issue_32611
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string hostname = Dns.GetHostName();
+            Console.WriteLine("My hostname is '{0}'", hostname);
+            Console.WriteLine("My ip addresses are:");
+            foreach (var address in Dns.GetHostAddresses(hostname)) {
+                Console.WriteLine("\t{0}", address);
+            }
+        }
+    }
+}
