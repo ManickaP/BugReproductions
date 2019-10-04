a09087b5b8278538c2fb681b0ccf159f308299e1
manicka <mapichov@microsoft.com>
2019-10-04 11:26:19 +0200

    22048 repro

---
+++ issue_22048/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Net.NetworkInformation;
+
+namespace issue_22048
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try {
+                if (NetworkInterface.GetIsNetworkAvailable()) {
+                    Console.WriteLine("Available!");
+                } else {
+                    Console.WriteLine("Unavailable!");
+                }
+            } catch (Exception) {
+                Console.WriteLine("Crashed!");
+            }
+        }
+    }
+}
