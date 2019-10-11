01ab4818f52613262b00500d647c22d2e536f8ca
Manicka <mapichov@microsoft.com>
2019-10-11 16:14:50 +0200

    Run GetHostName in loop

--- issue_32611/CSharpTests/Program.cs
+++ issue_32611/CSharpTests/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Net;
 using System.IO;
+using System.Diagnostics;
 
 namespace issue_32611
 {
@@ -8,11 +9,14 @@ namespace issue_32611
     {
         static void Main(string[] args)
         {
-            string hostname = Dns.GetHostName();
-            Console.WriteLine("My hostname is '{0}'", hostname);
-            Console.WriteLine("My ip addresses are:");
-            foreach (var address in Dns.GetHostAddresses(hostname)) {
-                Console.WriteLine("\t{0}", address);
+            var pid = Process.GetCurrentProcess().Id;
+            for (int i = 0; i < 100_000_000; ++i) {
+                string hostname = Dns.GetHostName();
+                Console.WriteLine("My hostname is '{0}' in '{1}'", hostname, pid);
+                Console.WriteLine("My ip addresses are:");
+                foreach (var address in Dns.GetHostAddresses(hostname)) {
+                    Console.WriteLine("\t{0}", address);
+                }
             }
         }
     }
