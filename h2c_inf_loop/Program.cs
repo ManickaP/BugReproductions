5aa21a46b1a3ee348d47297d0ab1a0a6144fe47e
Marie Píchová <mapichov@microsoft.com>
2020-09-21 10:38:24 +0200

    h2c test

---
+++ h2c_inf_loop/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace h2c_inf_loop
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            using var client = new HttpClient() {
+                DefaultVersionPolicy = HttpVersionPolicy.RequestVersionExact,
+                DefaultRequestVersion = HttpVersion.Version20,
+                Timeout = TimeSpan.FromDays(1)
+            };
+            try {
+                var _ = await client.GetAsync("http://corefx-net-http11.azurewebsites.net/echo.ashx");
+            } catch {}
+            var response = await client.GetAsync("http://corefx-net-http11.azurewebsites.net/echo.ashx");
+        }
+    }
+}
