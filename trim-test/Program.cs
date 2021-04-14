3777ed2e57ba1310b5047ec6b4488ffd329517f4
Marie Píchová <mapichov@microsoft.com>
2021-04-14 13:34:29 +0200

    trim test

---
+++ trim-test/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace trim_test
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var x = Process.GetCurrentProcess();
+            Console.WriteLine(OperatingSystem.IsLinux());
+            using var client = new HttpClient();
+            using var response = await client.GetAsync("https://www.microsoft.com");
+            var result = await response.Content.ReadAsStringAsync();
+            Console.WriteLine(result);
+        }
+    }
+}
