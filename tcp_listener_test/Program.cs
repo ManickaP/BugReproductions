be1e1727dfb7601a4deae17d38d4e93b74c5e76c
Marie Píchová <mapichov@microsoft.com>
2021-04-20 14:06:10 +0200

    TcpListener test

---
+++ tcp_listener_test/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+
+namespace tcp_listener_test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var x = new TcpListener(IPAddress.Loopback, 0);
+            Console.WriteLine(x.LocalEndpoint);
+            x.Start();
+            Console.WriteLine(x.LocalEndpoint);
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
