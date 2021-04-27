9c97ca10b2b19d32b6fe61d95dd1c313514436f7
Marie Píchová <mapichov@microsoft.com>
2021-04-27 20:59:34 +0200

    Added playground project.

---
+++ playground/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace playground
+{
+    class Program
+    {
+        static unsafe void Main(string[] args)
+        {
+            Console.WriteLine(new SafeMsQuicConnectionHandle().DangerousGetHandle());
+            byte* array = stackalloc byte[10];
+            
+            Console.WriteLine(new IntPtr(array));
+        }
+    }
+
+    
+    internal sealed class SafeMsQuicConnectionHandle : SafeHandle
+    {
+        public override bool IsInvalid => handle == IntPtr.Zero;
+
+        public SafeMsQuicConnectionHandle()
+            : base(IntPtr.Zero, ownsHandle: true)
+        { }
+
+        public SafeMsQuicConnectionHandle(IntPtr connectionHandle)
+            : this()
+        {
+            SetHandle(connectionHandle);
+        }
+
+        protected override bool ReleaseHandle()
+        {
+            return true;
+        }
+    }
+}
