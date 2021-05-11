551483af8a561812b8cac9cc468a731428aacc44
Marie Píchová <mapichov@microsoft.com>
2021-05-11 09:52:01 +0200

    test

--- playground/Program.cs
+++ playground/Program.cs
@@ -1,11 +1,20 @@
 ﻿using System;
+using System.Net.Http;
 using System.Runtime.InteropServices;
+using System.Threading.Tasks;
 
 namespace playground
 {
     class Program
     {
-        static unsafe void Main(string[] args)
+        static async Task Main(string[] args)
+        {
+            using var client = new HttpClient();
+            using var response = await client.SendAsync(new HttpRequestMessage(HttpMethod.Get, "https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Length"));
+            Console.WriteLine(response);
+            Console.WriteLine(response.Content.Headers.ContentLength);
+        }
+        static unsafe void Main1(string[] args)
         {
             Console.WriteLine(new SafeMsQuicConnectionHandle().DangerousGetHandle());
             byte* array = stackalloc byte[10];
