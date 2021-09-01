6731f246402564a5f5ab0a8f7a635ce75669e85f
ManickaP <mapichov@microsoft.com>
2021-09-01 18:59:36 +0200

    http3

---
+++ http3/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Net;
+
+Console.WriteLine("Hello, World!");
+var client = new HttpClient()
+{
+    DefaultRequestVersion = HttpVersion.Version30,
+    DefaultVersionPolicy = HttpVersionPolicy.RequestVersionOrHigher
+};
+var resp = await client.GetAsync("https://cloudflare-quic.com/");
+Console.WriteLine($"status: {resp.StatusCode}, version: {resp.Version}");
