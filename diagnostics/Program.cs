d83810e5f144e63b6b933215520a9d656fc03f98
ManickaP <mapichov@microsoft.com>
2021-11-11 17:36:46 +0100

    diag

--- diagnostics/Program.cs
+++ diagnostics/Program.cs
@@ -1,11 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.Diagnostics;
 using System.Net;
 
 using var listener = new HttpEventListener();
 
 Console.WriteLine("Hello, World!");
 
-var client = new HttpClient();
-var resp = await client.GetAsync("https://github.com");
+var client = new HttpClient(new SocketsHttpHandler() {
+    ActivityHeadersPropagator= DistributedContextPropagator.CreatePassThroughPropagator()
+});
+using Activity parent = new Activity("parent");
+parent.SetIdFormat(ActivityIdFormat.Hierarchical);
+parent.Start();
+var resp = await client.GetAsync("https://motherfuckingwebsite.com/");
 Console.WriteLine($"status: {resp.StatusCode}, version: {resp.Version}");
-
