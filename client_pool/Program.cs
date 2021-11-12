6866a7a289118fe0c4970ea03dedada2f8000e0f
ManickaP <mapichov@microsoft.com>
2021-11-12 21:57:25 +0100

    timer_test

--- client_pool/Program.cs
+++ client_pool/Program.cs
@@ -5,9 +5,7 @@ using var client = new HttpClient(new SocketsHttpHandler() {
     PooledConnectionIdleTimeout = TimeSpan.FromMilliseconds(1)
 });
 
-var x = File.ReadAllLines("/home/manicka/downloads/top500Domains.csv");
-
-var list = File.ReadAllLines("/home/manicka/downloads/top500Domains.csv").Skip(1).Select(line => "https://" + line.Split(',')[1].Trim('"')).ToArray();
+var list = File.ReadAllLines("top500Domains.csv").Skip(1).Select(line => "https://" + line.Split(',')[1].Trim('"')).ToArray();
 int i = 0;
 while (true) {
     try {
