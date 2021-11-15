210e6db88b565498fe565eab6d4d6fb9560a257e
ManickaP <mapichov@microsoft.com>
2021-11-15 19:18:01 +0100

    timer

--- timer_test/Program.cs
+++ timer_test/Program.cs
@@ -1,30 +1,40 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
-int _removeStalePoolsIsRunning = 0;
+// int _removeStalePoolsIsRunning = 0;
+
+// void RemoveStalePools()
+// {
+//     Console.WriteLine($"RemoveStalePools[{Thread.CurrentThread.ManagedThreadId:00000000}] called");
+//     // Check whether the method is not already running and prevent parallel execution.
+//     if (Interlocked.CompareExchange(ref _removeStalePoolsIsRunning, 1, 0) != 0)
+//     {
+//         Console.WriteLine($"RemoveStalePools[{Thread.CurrentThread.ManagedThreadId:00000000}] bailed");
+//         return;
+//     }
+//     Console.WriteLine($"RemoveStalePools[{Thread.CurrentThread.ManagedThreadId:00000000}] entered");
+
+//     try
+//     {
+//         Thread.Sleep(3000);
+//     }
+//     finally
+//     {
+//         Console.WriteLine($"RemoveStalePools[{Thread.CurrentThread.ManagedThreadId:00000000}] back to 0");
+//         // Make sure the guard value gets always reset back to 0 and that it's visible to other threads.
+//         Volatile.Write(ref _removeStalePoolsIsRunning, 0);
+//     }
+// }
+
+// Timer _cleaningTimer = new Timer((_) => RemoveStalePools(), null, TimeSpan.FromMilliseconds(1000), TimeSpan.FromMilliseconds(1000));
+// Thread.Sleep(TimeSpan.FromSeconds(15));
 
 void RemoveStalePools()
 {
     Console.WriteLine($"RemoveStalePools[{Thread.CurrentThread.ManagedThreadId:00000000}] called");
-    // Check whether the method is not already running and prevent parallel execution.
-    if (Interlocked.CompareExchange(ref _removeStalePoolsIsRunning, 1, 0) != 0)
-    {
-        Console.WriteLine($"RemoveStalePools[{Thread.CurrentThread.ManagedThreadId:00000000}] bailed");
-        return;
-    }
-    Console.WriteLine($"RemoveStalePools[{Thread.CurrentThread.ManagedThreadId:00000000}] entered");
-
-    try
-    {
-        Thread.Sleep(3000);
-    }
-    finally
-    {
-        Console.WriteLine($"RemoveStalePools[{Thread.CurrentThread.ManagedThreadId:00000000}] back to 0");
-        // Make sure the guard value gets always reset back to 0 and that it's visible to other threads.
-        Volatile.Write(ref _removeStalePoolsIsRunning, 0);
-    }
 }
 
-Timer _cleaningTimer = new Timer((_) => RemoveStalePools(), null, TimeSpan.FromMilliseconds(1000), TimeSpan.FromMilliseconds(1000));
+Timer _cleaningTimer = new Timer((_) => RemoveStalePools(), null, TimeSpan.FromMilliseconds(1000), Timeout.InfiniteTimeSpan);
+Thread.Sleep(1010);
+_cleaningTimer.Change(TimeSpan.FromSeconds(2), Timeout.InfiniteTimeSpan);
 Thread.Sleep(TimeSpan.FromSeconds(15));
