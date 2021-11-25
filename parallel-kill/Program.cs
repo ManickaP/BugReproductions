19af2fe78201423a5ce827bec2845f49b579f1dd
ManickaP <mapichov@microsoft.com>
2021-11-25 16:43:35 +0100

    parallel-kill

---
+++ parallel-kill/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+var cts = new CancellationTokenSource(TimeSpan.FromSeconds(5));
+cts.Token.Register(() => throw new Exception("Kill!"));
+
+while (true) {
+    await Task.Delay(TimeSpan.FromSeconds(1));
+    Console.WriteLine("Poop");
+}
