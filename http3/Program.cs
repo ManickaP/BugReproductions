b57a9fb3f6b51a9e95126fa02a361353ea9d7661
ManickaP <mapichov@microsoft.com>
2021-12-03 10:25:55 +0100

    h3

--- http3/Program.cs
+++ http3/Program.cs
@@ -1,11 +1,36 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.Diagnostics.Tracing;
 using System.Net;
+using System.Text;
 
+using var _ = new HttpEventListener();
 Console.WriteLine("Hello, World!");
 var client = new HttpClient()
 {
     DefaultRequestVersion = HttpVersion.Version30,
-    DefaultVersionPolicy = HttpVersionPolicy.RequestVersionOrHigher
+    DefaultVersionPolicy = HttpVersionPolicy.RequestVersionExact
 };
-var resp = await client.GetAsync("https://cloudflare-quic.com/");
+var resp = await client.GetAsync("https://msquic.net/");
 Console.WriteLine($"status: {resp.StatusCode}, version: {resp.Version}");
+
+internal sealed class HttpEventListener : EventListener
+{
+    protected override void OnEventSourceCreated(EventSource eventSource)
+    {
+        if (eventSource.Name == "Private.InternalDiagnostics.System.Net.Quic" ||
+            eventSource.Name == "Private.InternalDiagnostics.System.Net.Http")
+            EnableEvents(eventSource, EventLevel.LogAlways);
+    }
+
+    protected override void OnEventWritten(EventWrittenEventArgs eventData)
+    {
+        var sb = new StringBuilder().Append($"{eventData.TimeStamp:HH:mm:ss.fffffff}[{eventData.EventName}] ");
+        for (int i = 0; i < eventData.Payload?.Count; i++)
+        {
+            if (i > 0)
+                sb.Append(", ");
+            sb.Append(eventData.PayloadNames?[i]).Append(": ").Append(eventData.Payload[i]);
+        }
+        Console.WriteLine(sb.ToString());
+    }
+}
