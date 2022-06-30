e981c3c200e79c6ba4e5a33cf3f05b21c8cf7d80
ManickaP <mapichov@microsoft.com>
2022-06-30 21:47:32 +0200

    tests

---
+++ quic-test/Program.cs
@@ -0,0 +1,62 @@
+﻿using System.Diagnostics.Tracing;
+using System.Net;
+using System.Net.Quic;
+using System.Net.Security;
+using System.Reflection;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+using var httpListener = new HttpEventListener();
+
+string certificatePath = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)!, "testservereku.contoso.com.pfx");
+X509Certificate2 serverCertificate = new X509Certificate2(File.ReadAllBytes(certificatePath), "testcertificate", X509KeyStorageFlags.Exportable);
+
+var listener = await QuicListener.ListenAsync(new QuicListenerOptions() {
+    ListenEndPoint = new IPEndPoint(IPAddress.Loopback, 0),
+    ServerAuthenticationOptions = new SslServerAuthenticationOptions() {
+        ApplicationProtocols = new List<SslApplicationProtocol>() { SslApplicationProtocol.Http3 },
+        ServerCertificate = serverCertificate
+    },
+    IdleTimeout = TimeSpan.FromSeconds(5)
+});
+
+var connection = await QuicConnection.ConnectAsync(new QuicClientConnectionOptions(){
+    ClientAuthenticationOptions = new SslClientAuthenticationOptions() {
+        ApplicationProtocols = new List<SslApplicationProtocol>() { SslApplicationProtocol.Http3 },
+        RemoteCertificateValidationCallback = delegate { return true; }
+    },
+    RemoteEndPoint = listener.ListenEndPoint
+});
+await connection.ConnectAsync();
+var serverConnection = await listener.AcceptConnectionAsync();
+
+await connection.CloseAsync(123);
+connection.Dispose();
+Thread.Sleep(TimeSpan.FromSeconds(10));
+
+internal sealed class HttpEventListener : EventListener
+{
+
+    protected override void OnEventSourceCreated(EventSource eventSource)
+    {
+        // Allow internal Quic logging
+        if (eventSource.Name == "Private.InternalDiagnostics.System.Net.Quic")
+        {
+            EnableEvents(eventSource, EventLevel.LogAlways);
+        }
+    }
+
+    protected override void OnEventWritten(EventWrittenEventArgs eventData)
+    {
+        var sb = new StringBuilder().Append($"{eventData.TimeStamp:HH:mm:ss.fffffff}[{eventData.EventName}]{eventData.ActivityId}.{eventData.RelatedActivityId} ");
+        for (int i = 0; i < eventData.Payload?.Count; i++)
+        {
+            if (i > 0)
+                sb.Append(", ");
+            sb.Append(eventData.PayloadNames?[i]).Append(": ").Append(eventData.Payload[i]);
+        }
+        try {
+            Console.WriteLine(sb.ToString());
+        } catch { }
+    }
+}
--- socket_test/Program.cs
+++ socket_test/Program.cs
@@ -9,69 +9,69 @@ using System.Threading.Tasks;
 
 namespace socket_test
 {
-    class Program
+class Program
+{
+    static async Task Main(string[] args)
     {
-        static async Task Main(string[] args)
-        {
-            await Task.WhenAll(RunServer(), RunClient());
-        }
 
-        static readonly TaskCompletionSource<IPEndPoint> serverEndpoint = new TaskCompletionSource<IPEndPoint>();
-        static readonly TaskCompletionSource clientWrite = new TaskCompletionSource();
-
-        static async Task RunClient()
-        {
-            var socket = new Socket(AddressFamily.InterNetworkV6, SocketType.Stream, ProtocolType.Tcp);
-            socket.NoDelay = true;
-            var endpoint = await serverEndpoint.Task;
-            Console.WriteLine("Client connecting to: " + endpoint);
-            socket.Connect(endpoint);
-            Console.WriteLine("Client connected to: " + socket.RemoteEndPoint);
-            var stream = new NetworkStream(socket, ownsSocket: true);
-            for (int i = 0; i < 1_000; ++i)
-            {
-                await stream.WriteAsync(UTF8Encoding.UTF8.GetBytes("Ahoj"));
-            }
-            clientWrite.SetResult();
-            /*try
-            {
-                await stream.ReadAsync(new byte[10]);
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex);
-            }*/
-            socket.Shutdown(SocketShutdown.Both);
-            await stream.DisposeAsync();
-            /*var buffer = new byte[100];
-            int readBytes;
-            while ((readBytes = await stream.ReadAsync(buffer)) > 0)
-            {
-                Console.WriteLine("Client:" + UTF8Encoding.UTF8.GetString(buffer, 0, readBytes));
-            }
-            Console.WriteLine("Client:" + readBytes);*/
-        }
-        static async Task RunServer()
-        {
-            var listenSocket = new Socket(SocketType.Stream, ProtocolType.Tcp);
-            listenSocket.Bind(new IPEndPoint(IPAddress.IPv6Any, 0));
-            listenSocket.Listen();
-            serverEndpoint.SetResult(listenSocket.LocalEndPoint as IPEndPoint);
-            Console.WriteLine("Server listening on: " + listenSocket.LocalEndPoint);
-            var socket = await listenSocket.AcceptAsync().ConfigureAwait(false);
-            socket.NoDelay = true;
-            socket.LingerState = new LingerOption(true, 0);
-            var stream = new NetworkStream(socket, ownsSocket: true);
-            var buffer = new byte[100];
-            int readBytes = 0;
-            do
-            {
-                readBytes = await stream.ReadAsync(buffer);
-                Console.WriteLine($"Server({readBytes}):" + UTF8Encoding.UTF8.GetString(buffer, 0, readBytes));
-                await clientWrite.Task;
-                socket.Close(0);
-            } while (false);//(readBytes > 0);
-            stream.Dispose();
-        }
+        Console.WriteLine("{0:X8}",
+            unchecked(
+                (int)(5023) <= 0 ? ((int)(5023)) : ((int)(((5023) & 0x0000FFFF) | (7 << 16) | 0x80000000))
+                )
+        );
+        await Task.WhenAll(RunServer(), RunClient());
     }
+
+    static readonly TaskCompletionSource<IPEndPoint> serverEndpoint = new TaskCompletionSource<IPEndPoint>();
+    static readonly TaskCompletionSource clientWrite = new TaskCompletionSource();
+
+    static async Task RunClient()
+    {
+        var socket = new Socket(SocketType.Stream, ProtocolType.Tcp);
+        socket.NoDelay = true;
+        var endpoint = await serverEndpoint.Task;
+        Console.WriteLine("Client connecting to: " + endpoint);
+        socket.Connect(endpoint);
+        Console.WriteLine("Client connected to: " + socket.RemoteEndPoint);
+        var stream = new NetworkStream(socket, ownsSocket: true);
+        for (int i = 0; i < 1_000; ++i)
+        {
+            await stream.WriteAsync(UTF8Encoding.UTF8.GetBytes("Ahoj"));
+        }
+        clientWrite.SetResult();
+        /*try
+        {
+            // Read will throw "Connection reset by peer".
+            await stream.ReadAsync(new byte[10]);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex);
+        }*/
+        socket.Shutdown(SocketShutdown.Send);
+        stream.Dispose();
+    }
+    static async Task RunServer()
+    {
+        var listenSocket = new Socket(SocketType.Stream, ProtocolType.Tcp);
+        listenSocket.Bind(new IPEndPoint(IPAddress.Loopback, 0));
+        listenSocket.Listen();
+        serverEndpoint.SetResult(listenSocket.LocalEndPoint as IPEndPoint);
+        Console.WriteLine("Server listening on: " + listenSocket.LocalEndPoint);
+        var socket = await listenSocket.AcceptAsync().ConfigureAwait(false);
+        socket.NoDelay = true;
+        // To force RST when calling Close.
+        socket.LingerState = new LingerOption(true, 0);
+        var stream = new NetworkStream(socket, ownsSocket: true);
+        var buffer = new byte[100];
+        int readBytes = 0;
+        do
+        {
+            readBytes = await stream.ReadAsync(buffer);
+            Console.WriteLine($"Server({readBytes}):" + UTF8Encoding.UTF8.GetString(buffer, 0, readBytes));
+            await clientWrite.Task;
+            socket.Close(0);
+        } while (false);//(readBytes > 0);
+    }
+}
 }
