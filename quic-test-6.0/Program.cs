8038b88023c2ea05fa45f1938c9d7481c2a19492
Marie Píchová <mapichov@microsoft.com>
2023-01-19 14:54:54 +0100

    repro

---
+++ quic-test-6.0/Program.cs
@@ -0,0 +1,7 @@
+﻿using System.Net;
+using System.Net.Quic;
+using System.Net.Security;
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine(QuicImplementationProviders.MsQuic.IsSupported);
+Console.WriteLine("Hello, World!");
