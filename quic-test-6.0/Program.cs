7065c437ba8bd5a1e4ccf58f438ab00ae72d2d2d
ManickaP <mapichov@microsoft.com>
2023-01-19 18:50:23 +0100

    test

--- quic-test-6.0/Program.cs
+++ quic-test-6.0/Program.cs
@@ -1,7 +1,9 @@
-﻿using System.Net;
+﻿using System;
+using System.Net;
 using System.Net.Quic;
 using System.Net.Security;
 
 // See https://aka.ms/new-console-template for more information
 Console.WriteLine(QuicImplementationProviders.MsQuic.IsSupported);
+Console.WriteLine(Environment.ProcessId);
 Console.WriteLine("Hello, World!");
