493a42b8c58680a645e8a25f7b27dc66f102e8ad
ManickaP <mapichov@microsoft.com>
2023-10-12 10:51:11 +0200

    test

--- playground-2/Program.cs
+++ playground-2/Program.cs
@@ -2,14 +2,27 @@
 using System.Net.Quic;
 using System.Net.Security;
 using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
 using System.Security.Cryptography.X509Certificates;
 
 // See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
+unsafe {
+    void* pointer = NativeMemory.AllocZeroed(5);
+    Console.WriteLine("p :" + (nint)pointer);
+    void* original = (void*)Interlocked.Exchange(ref *(nint*)&pointer, 0);
+    Console.WriteLine("o1:" + (nint)original);
+    Console.WriteLine("p1:" + (nint)pointer);
+    original = (void*)Interlocked.Exchange(ref *(nint*)&pointer, IntPtr.Zero);
+    Console.WriteLine("o2:" + (nint)original);
+    Console.WriteLine("p2:" + (nint)pointer);
+}
+/*
 Console.WriteLine(await new HttpClient().GetAsync("https://httpstat.us/400"));
 
-//using var testListener = new HttpEventListener();
+using var testListener = new HttpEventListener();
 
 string certificatePath = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)!, "testservereku.contoso.com.pfx");
 X509Certificate2 serverCertificate = new X509Certificate2(File.ReadAllBytes(certificatePath), "testcertificate", X509KeyStorageFlags.Exportable);
@@ -69,7 +82,7 @@ static Task<HttpResponseMessage> HttpReq(CookieContainer cookieContainer)
 
     client.Timeout = TimeSpan.FromSeconds(3000);
     // add default headers
-    client.DefaultRequestHeaders.TryAddWithoutValidation("Accept", "text/javascript, text/html, application/xml, text/xml, */*");
+    client.DefaultRequestHeaders.TryAddWithoutValidation("Accept", "text/javascript, text/html, application/xml, text/xml");
     client.DefaultRequestHeaders.TryAddWithoutValidation("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/109.0");
     client.DefaultRequestHeaders.TryAddWithoutValidation("Accept-Encoding", "gzip, deflate, br");
     client.DefaultRequestHeaders.TryAddWithoutValidation("Accept-Language", "uk-UA,uk;q=0.8,en-US;q=0.5,en;q=0.3");
@@ -103,3 +116,4 @@ foreach (Cookie new_cookie in responseCookies)
 {
     Console.WriteLine(new_cookie.Name + "=" + new_cookie.Value + "; " + new_cookie.Domain + "; " + new_cookie.Path);
 }
+*/
